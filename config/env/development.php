ced1a7b186226d773c83c5fd5145bef879770aeb
Minh Tri <minhtri.contact@gmail.com>
2018-01-08 08:02:27 +0700

    rename `envvars` to `env`

--- config/env/development.php
+++ config/env/development.php
@@ -3,8 +3,6 @@
  * Debugging
  *
  * @see  https://codex.wordpress.org/Debugging_in_WordPress
- *
- * @var  bool
  */
 define('WP_DEBUG', true);
 
@@ -12,8 +10,6 @@ define('WP_DEBUG', true);
  * Save queries for analysis
  *
  * @see  https://codex.wordpress.org/Editing_wp-config.php#Save_queries_for_analysis
- *
- * @var  bool
  */
 define('SAVEQUERIES', true);
 
@@ -21,7 +17,5 @@ define('SAVEQUERIES', true);
  * Script debugging
  *
  * @see  https://codex.wordpress.org/Debugging_in_WordPress
- *
- * @var  bool
  */
 define('SCRIPT_DEBUG', true);
--- config/env/staging.php
+++ config/env/staging.php
@@ -3,15 +3,11 @@
  * Debugging
  *
  * @see  https://codex.wordpress.org/Debugging_in_WordPress
- *
- * @var  bool
  */
 define('WP_DEBUG', false);
 
 /**
  * Error logging
- *
- * Please make sure that `error.log` is writable by server agent.
  *
  * @see  https://codex.wordpress.org/Editing_wp-config.php#Configure_Error_Logging
  */
@@ -24,8 +20,6 @@ define('WP_DEBUG', false);
  * Disable theme and plugin editor
  *
  * @see  https://codex.wordpress.org/Editing_wp-config.php#Disable_the_Plugin_and_Theme_Editor
- *
- * @var  bool
  */
 define('DISALLOW_FILE_EDIT', true);
 
@@ -33,7 +27,5 @@ define('DISALLOW_FILE_EDIT', true);
  * Disable theme|plugin update and installation
  *
  * @see  https://codex.wordpress.org/Editing_wp-config.php#Disable_Plugin_and_Theme_Update_and_Installation
- *
- * @var  bool
  */
 define('DISALLOW_FILE_MODS', true);
